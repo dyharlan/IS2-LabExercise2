b6185d0da5e97333161be63e8d134634034d3eac
dyharlan <dyharlan45@gmail.com>
2023-09-26 18:24:29 +0800

    Update experiments

--- diabetes_tree.py
+++ diabetes_tree.py
@@ -11,7 +11,7 @@ import numpy as np
 df = pd.read_csv("diabetes.csv")
 
 from sklearn.model_selection import train_test_split
-#
+### Split the data set
 X = df[['BloodPressure', 'Age', 'Insulin', 'Glucose', 'BMI']]
 y = df['Outcome']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
