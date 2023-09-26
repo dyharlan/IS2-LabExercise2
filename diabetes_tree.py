5c9f9bf3a205db23aec7c3a9cdba5f79d2c6c094
dyharlan <dyharlan45@gmail.com>
2023-09-26 18:04:16 +0800

    Print model performance on both training and test sets

--- diabetes_tree.py
+++ diabetes_tree.py
@@ -11,6 +11,7 @@ import numpy as np
 df = pd.read_csv("diabetes.csv")
 
 from sklearn.model_selection import train_test_split
+#
 X = df[['BloodPressure', 'Age', 'Insulin', 'Glucose', 'BMI']]
 y = df['Outcome']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -20,11 +21,19 @@ clf = DecisionTreeClassifier(random_state=0, criterion='entropy', max_depth=3, m
 clf.fit(X_train, y_train)
 
 ### Qualitative Evaluation
-y_preds = clf.predict(X_test)
-print("Accuracy %.4f" % accuracy_score(y_test, y_preds))
-print("Precision %.4f" % precision_score(y_test, y_preds))
-print("Recall %.4f" % recall_score(y_test, y_preds))
-print("F1 %.4f" % f1_score(y_test, y_preds))
+test_set_y_preds = clf.predict(X_test)
+print("Model performance on test set: ")
+print("Accuracy %.4f" % accuracy_score(y_test, test_set_y_preds))
+print("Precision %.4f" % precision_score(y_test, test_set_y_preds))
+print("Recall %.4f" % recall_score(y_test, test_set_y_preds))
+print("F1 %.4f" % f1_score(y_test, test_set_y_preds))
+
+print("\nModel performance on training set: ")
+training_set_y_preds = clf.predict(X_train)
+print("Accuracy %.4f" % accuracy_score(y_train, training_set_y_preds))
+print("Precision %.4f" % precision_score(y_train, training_set_y_preds))
+print("Recall %.4f" % recall_score(y_train, training_set_y_preds))
+print("F1 %.4f" % f1_score(y_train, training_set_y_preds))
 
 ### Display the current tree
 plot_tree(clf, feature_names = list(X.columns), class_names = ['No Diabetes', 'Diabetes'],filled = True)
